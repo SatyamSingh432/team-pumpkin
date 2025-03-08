3031cb241c2f92981f0fbbed5d53325ee6e804d7
Satyam Singh <satyamrajput7239@gmail.com>
2025-03-08 19:22:25 +0530

    feat: frontend apis modify

--- Frontend/src/utils/apis.js
+++ Frontend/src/utils/apis.js
@@ -1,14 +1,26 @@
-const URL = "https://localhost:8082";
+const URL = "http://localhost:8082";
 
-const useregister = async (username, password) => {
-  const response = await fetch(`${URL}/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username: username, password: password }),
-  });
-
-  const resJson = await response.json();
-  localStorage.setItem("token", resJson.token);
-  return resJson;
+const useregister = async (username, email, number, password) => {
+  try {
+    const response = await fetch(`${URL}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        number: number,
+        password: password,
+      }),
+    });
+    const resJson = await response.json();
+    if (!response.ok) {
+      return { message: "User already exist" };
+    }
+    localStorage.setItem("token", resJson.token);
+    return resJson;
+  } catch (error) {
+    console.error("API Error:", error);
+    return { error: true, message: error.message };
+  }
 };
 export { useregister };
