8b249d123ee3968c02fc0ee541d4b06b2e2c46af
Satyam Singh <satyamrajput7239@gmail.com>
2025-03-08 19:20:15 +0530

    feat: frontend add login and register

--- Frontend/src/components/InputBox.jsx
+++ Frontend/src/components/InputBox.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import "../styles/InputBox.css";
-const InputBox = ({ placeholder, type }) => {
+const InputBox = ({ placeholder, type, name, onChange, value }) => {
   return (
     <input
       className="input-box-welcome-page"
       type={type}
+      name={name}
       placeholder={placeholder}
+      onChange={onChange}
+      value={value}
       required
     />
   );
--- Frontend/src/pages/WelcomePage.jsx
+++ Frontend/src/pages/WelcomePage.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import InputBox from "../components/InputBox";
 import Button from "../components/Butoon";
@@ -6,19 +7,82 @@ import Button from "../components/Butoon";
 import logo from "../assets/image66.png";
 import logo2 from "../assets/DottedShape.png";
 
+import { useregister } from "../utils/apis";
 import "../styles/WelcomePage.css";
 
 const WelcomePage = () => {
   const inputFields = [
-    { placeholder: "Enter Name", type: "text" },
-    { placeholder: "Email", type: "email" },
-    { placeholder: "Phone Number", type: "number" },
+    { placeholder: "Enter Name", type: "text", name: "username" },
+    { placeholder: "Email", type: "email", name: "email" },
+    { placeholder: "Phone Number", type: "number", name: "number" },
+    {
+      placeholder: "Password",
+      type: "password",
+      name: "password",
+    },
+    {
+      placeholder: "Confirm-Password",
+      type: "password",
+      name: "confirmpassword",
+    },
   ];
-  const navigate = useNavigate();
-  const submitHandler = (evt) => {
-    evt.preventDefault();
-    navigate("/chatpage");
+  const [formData, setFormData] = useState({
+    username: "satyam vaaa",
+    email: "satyamrajput7239@gmail.com",
+    number: "1233",
+    password: "cxcx",
+    confirmpassword: "cxcx",
+  });
+  const [isLogin, setIsLogin] = useState(false);
+
+  // const navigate = useNavigate();
+  const changeHandler = (evt) => {
+    setFormData((prvData) => ({
+      ...prvData,
+      [evt.target.name]: evt.target.value,
+    }));
   };
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+    // navigate("/chatpage");
+    if (!isLogin) {
+      console.log(
+        formData.username,
+        formData.email,
+        formData.number,
+        formData.password
+      );
+      try {
+        const res = await useregister(
+          formData.username,
+          formData.email,
+          formData.number,
+          formData.password
+        );
+        if (res.message) {
+          alert(res.message);
+          return;
+        }
+        alert("Registration Successful");
+        console.log(res);
+      } catch (error) {
+        alert("An error occurred while registering.");
+        console.error("Registration Error", error);
+      }
+
+      console.log(formData);
+    } else {
+      console.log(formData);
+    }
+    setFormData({
+      username: "",
+      email: "",
+      number: "",
+      password: "",
+      confirmpassword: "",
+    });
+  };
+
   return (
     <main className="welcome-page-container">
       <form className="login-register-container" onSubmit={submitHandler}>
@@ -32,18 +96,52 @@ const WelcomePage = () => {
         <div className="welcome-logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="welcome-page-input-container">
-          {inputFields.map((data, index) => {
-            return (
-              <InputBox
-                key={index}
-                placeholder={data.placeholder}
-                type={data.type}
-              />
-            );
-          })}
-        </div>
-        <Button buttonText="Log In" type="submit" />
+        {!isLogin ? (
+          <div className="welcome-page-input-container">
+            {inputFields.map((data, index) => {
+              return (
+                <InputBox
+                  key={index}
+                  placeholder={data.placeholder}
+                  type={data.type}
+                  onChange={changeHandler}
+                  name={data.name}
+                  value={formData[data.name]}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div className="welcome-page-input-container">
+            {inputFields
+              .filter((data) => {
+                return (
+                  data.placeholder === "Enter Name" ||
+                  data.placeholder === "Password"
+                );
+              })
+              .map((data, index) => {
+                return (
+                  <InputBox
+                    key={index}
+                    placeholder={data.placeholder}
+                    type={data.type}
+                    onChange={changeHandler}
+                    name={data.name}
+                    value={formData[data.name]}
+                  />
+                );
+              })}
+          </div>
+        )}
+
+        <Button buttonText={!isLogin ? "SIGN UP" : "LOG IN"} type="submit" />
+        <p className="toggle-text">
+          {isLogin ? "Don't have an account? " : "Already have an account? "}
+          <span onClick={() => setIsLogin(!isLogin)}>
+            {!isLogin ? "Login" : "Register"}
+          </span>
+        </p>
       </form>
     </main>
   );
